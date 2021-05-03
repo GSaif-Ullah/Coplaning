7fdb2b9684ed607beffe68e00bde1e50652327ff
GSaif-Ullah <g.saif-ullah@outlook.fr>
2021-05-03 15:58:37 +0200

    1

--- src/main/webapp/inscriptions.js
+++ src/main/webapp/inscriptions.js
@@ -29,25 +29,31 @@ $(function () {
 	            }                  
         };
         var password2=$("#password2").val();
+        var password2=$("#password2").val();
+        var a=data.passenger.firstname=="" ||  data.passenger.name==""  || data.passenger.password==""  || data.passenger.password2=="" || data.passenger.email=="" || data.passenger.phone==""  || data.passenger.birth=="";
+        var b=data.passenger.email.indexOf("@")<1 ||data.passenger.email.lastIndexOf(".")<data.passenger.email.indexOf("@")+2;
+        var c=data.passenger.password.length<8 ;
+        var d= data.passenger.password!=password2;
+        var e=gridCheck.checked==false;
         
-        if (data.passenger.firstname=="" ||  data.passenger.name==""  || data.passenger.password==""  || data.passenger.password2=="" || data.passenger.email=="" || data.passenger.phone==""  || data.passenger.birth==""){
+        if (a){
             alert("Veuillez vérifier les champs vides ");
         }
         
 	    else{
-			if(data.passenger.email.indexOf("@")<1 ||data.passenger.email.lastIndexOf(".")<data.passenger.email.indexOf("@")+2 ){
+			if(b ){
 				alert("Veuillez entrer une adresse mail valide");
 			}
 			
-			if (data.passenger.password.length<8){
-				alert("Erreur !Le mot de passe doit faire au moins 8 caractères ");
+			if (c){
+				alert("Le mot de passe doit faire au moins 8 caractères ");
 			} 
 		    
-			if (data.passenger.password!=password2){
-				alert("Erreur !Les mots de passe ne correspondent pas ");
+			if ( d){
+				alert("Les deux mots de passe ne correspondent pas ");
 			}
 				
-		    if (gridCheck.checked==false){
+		    if (e){
 		    	alert("Veuillez accepter les conditions d'utilisation");
 		    }
 		    else{				
@@ -55,7 +61,7 @@ $(function () {
 					if (result==true){
 					   alert("Adresse mail déjà utilisée");
 					}
-					else {
+					if (!a && !b && !c && !d && !e ) {
 					   putServerData("ws/passenger", data, function (result) {           
 					   alert("Inscription réussie, bonne réservation ! ");
 					   window.location.replace("home.html");
